cf6f41085b5122855413c46eea0f13f6fe0c0cf9
Seeagarg <89650047+Seeagarg@users.noreply.github.com>
2024-01-12 12:43:15 +0530

    last

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const user = require("../models/Users");
+const user = require("../Models/Users");
 const { body,validationResult } = require("express-validator");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
