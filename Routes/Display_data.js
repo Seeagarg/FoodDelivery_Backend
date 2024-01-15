b2c9d471b6cb7b20fc9f2c5890813f7e6fe490c7
Seeagarg <89650047+Seeagarg@users.noreply.github.com>
2024-01-16 00:02:05 +0530

    last

--- Routes/Display_data.js
+++ Routes/Display_data.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router()
 
 router.post('/foodData',(req,res)=>{
     try{
-        console.log(global.food_items)
-        res.send([global.food_items,global.FoodCategory])
+
+        const result = mongoose.connection.db.collection("Food_items");
+            result.find({}).toArray(function(err,data){
+                const foodCategory = mongoose.connection.db.collection("Food_Category");
+                foodCategory.find({}).toArray(function(err,catData){
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        console.log(data)
+                        console.log(catData)
+                        res.send([data,catData])
+                        
+                        // global.FoodCategory = catData;
+                        // global.food_items = data;
+                    }
+        
+                })
+
+            })
+
+
+
+        // console.log(global.food_items)
+        // console.log(global.FoodCategory)
+        // res.send([global.food_items,global.FoodCategory])
+
     }catch(error){
         console.error(error.message);
         res.send("server Error");
